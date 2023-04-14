a2a7c812eb4eaf2ed580bfc6194edcb94ea3276d
Damianopipitone <damianopipitone33@gmail.com>
2023-04-14 16:23:55 +0200

    Aggiungere i file di progetto.

---
+++ Bonus/Program.cs
@@ -0,0 +1,6 @@
+﻿// Chiedere all'utente quanti cibi preferiti ha
+// Stampare il cibo di mezza classifica 
+// Stampare l'indice mediano della classifica
+
+Console.WriteLine("Quanti cibi preferiti hai?");
+
---
+++ Miei cibi preferiti/Program.cs
@@ -0,0 +1,49 @@
+﻿// Inserire i cibi preferiti in un array (max 10)
+// Stampare la lunghezza della classifica
+// sgtampare tutto l'array (cibi classificati)
+// Cibo top
+// Cibo bottom
+
+string[] favouriteFood = { "Pizza", "Sushi", "Hamburger", "Patate", "Pasta", "Gelato" };
+
+classification(favouriteFood);
+
+lenght(favouriteFood);
+
+firstElement(favouriteFood);
+
+lastElement(favouriteFood);
+
+
+
+
+
+
+// ------- FUNZIONI -------
+
+void classification (string[] array)
+{ 
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine((i + 1) + "° - " + array[i]);
+    } 
+
+}
+
+void lenght (string[] array)
+{
+    for (int i = 0;i < array.Length; i++)
+    {
+        Console.WriteLine((i + 1));
+    }
+}
+
+void firstElement(string[] array)
+{
+    Console.WriteLine(array[0]);    
+}
+
+void lastElement(string[] array)
+{
+    Console.WriteLine(array[array.Length - 1]);
+}
